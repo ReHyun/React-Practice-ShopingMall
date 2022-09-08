cac5636d53a1907a1fac4676daaa85ab1038e7f5
ReHyun <guswogus81@naver.com>
2022-09-08 14:58:47 +0900

    2022-09-08 React

--- src/App.js
+++ src/App.js
@@ -11,13 +11,11 @@ import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import { DetailPage } from './DetailPage'
 import axios from 'axios'
 
-
-
 function App() {
 
   let [shoes, setShoes] = useState(data);
+  let [count, setCount] = useState(0);
   let navigate = useNavigate();
-
 
   return (
     <div className="App">
@@ -26,7 +24,7 @@ function App() {
           <Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={() => { navigate('/') }}> Home </Nav.Link>
-            <Nav.Link onClick={() => { navigate('/detail/') }}>Detail</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail/0') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -40,7 +38,7 @@ function App() {
                 {
                   shoes.map(function (a, i) {
                     return (
-                      <div className="col-md-4" onClick={()=>{navigate('/detail/'+i)}}>
+                      <div className="col-md-4" onClick={() => { navigate('/detail/' + i) }}>
                         <img src={"https://codingapple1.github.io/shop/shoes" + (i + 1) + ".jpg"} width="80%" />
                         <h4>{shoes[i].title}</h4>
                         <p>{shoes[i].price}</p>
@@ -49,30 +47,40 @@ function App() {
                   })
                 }
               </div>
-              <button onClick={()=>{
-                axios.get('https://codingapple1.github.io/shop/data2.json').then((result)=>{
-                  let arr = [...shoes];
-                  let newArr = arr.concat(result.data);
-                  setShoes(newArr);
-              })
-                .catch(()=>{
-                  console.log(' ajax 실패 ')
-                })
-
-              }}>Button</button>
+              <button onClick={() => {
+                setCount(count + 1);
+                if (count == 0) {
+                  axios.get("https://codingapple1.github.io/shop/data2.json").then((result) => {
+                    let arr = [...shoes];
+                    let newArr = arr.concat(result.data);
+                    setShoes(newArr);
+                  })
+                    .catch(() => {
+                      console.log(' ajax 실패 ');
+                    })
+                } else if (count == 1) {
+                  axios.get("https://codingapple1.github.io/shop/data3.json").then((result) => {
+                    let arr = [...shoes];
+                    let newArr = arr.concat(result.data);
+                    setShoes(newArr);
+                  })
+                    .catch(() => {
+                      console.log(' ajax 실패 ');
+                    })
+                }
+                else {
+                  alert("더 이상 상품이 없습니");
+                }
+              }
+              }>더보기</button>
             </div>
           </div>
         } />
-
         <Route path="/detail/:id" element={<DetailPage shoes={shoes}></DetailPage>} />
-
       </Routes>
 
     </div>
   );
 }
-
-
-
 
 export default App;
