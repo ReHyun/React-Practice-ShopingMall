f3fba97a2110fc5df474cb19218745ea938e2f64
ReHyun <guswogus81@naver.com>
2022-10-02 13:12:49 +0900

    2022-10-02 React

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { createContext, useEffect, useState } from 'react';
+import { lazy, Suspense, createContext, useEffect, useState } from 'react';
 import { data } from './data.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
-import { DetailPage } from './DetailPage'
 import axios from 'axios'
-import { Cart } from './Cart.js';
 import { useQuery } from 'react-query';
 
+//import { DetailPage } from './DetailPage'
+//import { Cart } from './Cart.js';
+
+const Detail = lazy(() => import('./routes/Detail.js'))
+const Cart = lazy(() => import('./routes/Cart.js'))
 
 function App() {
 
-  useEffect(()=>{
-    if (localStorage.getItem('watched').length == 0){
+  useEffect(() => {
+    if (localStorage.getItem('watched').length == 0) {
       localStorage.setItem('watched', JSON.stringify([]))
     }
-  },[])
+  }, [])
 
   let [shoes, setShoes] = useState(data);
   let [count, setCount] = useState(0);
@@ -28,7 +31,7 @@ function App() {
   let navigate = useNavigate();
 
   useQuery()
-  
+
 
   return (
     <div className="App">
@@ -45,58 +48,59 @@ function App() {
         </Container>
       </Navbar>
 
-      <Routes>
-        <Route path="/" element={
-          <div>
-            <div className="main-bg"></div>
-            <div className="container">
-              <div className="row">
-                {
-                  shoes.map(function (a, i) {
-                    return (
-                      <div className="col-md-4" onClick={() => { navigate('/detail/' + i); }}>
-                        <img src={"https://codingapple1.github.io/shop/shoes" + (i + 1) + ".jpg"} width="80%" />
-                        <h4>{shoes[i].title}</h4>
-                        <p>{shoes[i].price}</p>
-                      </div>
-                    )
-                  })
+      <Suspense fallback={<div>로딩중</div>}>
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {
+                    shoes.map(function (a, i) {
+                      return (
+                        <div className="col-md-4" onClick={() => { navigate('/detail/' + i); }}>
+                          <img src={"https://codingapple1.github.io/shop/shoes" + (i + 1) + ".jpg"} width="80%" />
+                          <h4>{shoes[i].title}</h4>
+                          <p>{shoes[i].price}</p>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+                <button onClick={() => {
+                  setCount(count + 1);
+                  if (count == 0) {
+                    axios.get("https://codingapple1.github.io/shop/data2.json").then((result) => {
+                      let arr = [...shoes];
+                      let newArr = arr.concat(result.data);
+                      setShoes(newArr);
+                    })
+                      .catch(() => {
+                        console.log(' ajax 실패 ');
+                      })
+                  } else if (count == 1) {
+                    axios.get("https://codingapple1.github.io/shop/data3.json").then((result) => {
+                      let arr = [...shoes];
+                      let newArr = arr.concat(result.data);
+                      setShoes(newArr);
+                    })
+                      .catch(() => {
+                        console.log(' ajax 실패 ');
+                      })
+                  }
+                  else {
+                    alert("더 이상 상품이 없습니");
+                  }
                 }
+                }>더보기</button>
               </div>
-              <button onClick={() => {
-                setCount(count + 1);
-                if (count == 0) {
-                  axios.get("https://codingapple1.github.io/shop/data2.json").then((result) => {
-                    let arr = [...shoes];
-                    let newArr = arr.concat(result.data);
-                    setShoes(newArr);
-                  })
-                    .catch(() => {
-                      console.log(' ajax 실패 ');
-                    })
-                } else if (count == 1) {
-                  axios.get("https://codingapple1.github.io/shop/data3.json").then((result) => {
-                    let arr = [...shoes];
-                    let newArr = arr.concat(result.data);
-                    setShoes(newArr);
-                  })
-                    .catch(() => {
-                      console.log(' ajax 실패 ');
-                    })
-                }
-                else {
-                  alert("더 이상 상품이 없습니");
-                }
-              }
-              }>더보기</button>
             </div>
-          </div>
-        } />
-        <Route path="/detail/:id" element={<DetailPage shoes={shoes}/>}/>
-        <Route path="/cart" element={<Cart/>}/>
+          } />
+          <Route path="/detail/:id" element={<DetailPage shoes={shoes} />} />
+          <Route path="/cart" element={<Cart />} />
 
-      </Routes>
-
+        </Routes>
+      </Suspense>
     </div >
   );
 }
